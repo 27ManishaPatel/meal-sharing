61927ef784b4d7fe07b2503b44d069ac6c8b0d8b
HackYourFuture DK <manisha270989@gmail.com>
2022-09-21 23:12:50 +0200

    added new route api/reviews

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,7 +3,11 @@ const app = express();
 const router = express.Router();
 const path = require("path");
 
+const knex = require("./database");
+
 const mealsRouter = require("./api/meals");
+const reservationsRouter = require("./api/reservations");
+const reviewsRouter = require("./api/reviews")
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
@@ -20,6 +24,63 @@ app.use(express.json());
 app.use(cors());
 
 router.use("/meals", mealsRouter);
+router.use("/reservations", reservationsRouter);
+router.use("/reviews", reviewsRouter)
+
+//after the line where it says router.use("/meals", mealsRouter);. There you can go ahead and define the desired routes like you normally would:
+
+app.get("/my-route", (req, res) => { res.send("Hi friend") });
+
+//Respond with all meals in the future (relative to the when datetime)
+app.get('/future-meals', async(req, res) => {
+  const dbResult = await knex.raw('SELECT * FROM meal WHERE when_date > NOW()')
+  const rows = dbResult[0]
+  if (rows.length === 0){
+    res.status(404).json({ status : 'No meals available'})
+  }else {
+    res.json(rows)
+  }
+});
+//Respond with all meals in the past (relative to the when datetime)
+app.get('/past-meals', async(req, res) => {
+  const dbResult = await knex.raw('SELECT * FROM meal WHERE when_date < NOW()')
+  const rows = dbResult[0]
+  if (rows.length === 0){
+    res.status(404).json({ status : 'No meals available'})
+  }else {
+    res.json(rows)
+  }
+})
+//Respond with all meals sorted by ID
+app.get('/all-meals', async(req, res) => {
+  const dbResult = await knex.raw('SELECT * FROM meal ORDER BY id')
+  const rows = dbResult[0]
+  if (rows.length === 0){
+    res.status(404).json({ status : 'No meals available'})
+  }else {
+    res.json(rows)
+  }
+})
+//Respond with the first meal (meaning with the minimum id)
+app.get('/first-meal', async(req, res) => {
+  const dbResult = await knex.raw('SELECT * FROM meal ORDER BY id LIMIT 1')
+  const rows = dbResult[0]
+  if (rows.length === 0){
+    res.status(404).json({ status : 'No meals available'})
+  }else {
+    res.json(rows)
+  }
+})
+//Respond with the last meal (meaning with the maximum id)
+app.get('/last-meal', async(req, res) => {
+  const dbResult = await knex.raw('SELECT * FROM meal ORDER BY id DESC LIMIT 1')
+  const rows = dbResult[0]
+  if (rows.length === 0){
+    res.status(404).json({ status : 'No meals available'})
+  }else {
+    res.json(rows)
+  }
+})
 
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
@@ -32,4 +93,8 @@ app.use("*", (req, res) => {
   res.sendFile(path.join(`${buildPath}/index.html`));
 });
 
+//added this to connecting port
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
 module.exports = app;
