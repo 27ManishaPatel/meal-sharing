e0c19d9af3f9c02a9e56d85f943a08f31c9990ab
HackYourFuture DK <manisha270989@gmail.com>
2022-11-01 12:33:04 +0100

    updated with package.json

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,37 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
-
+​
 function App() {
   return (
-    <Router>
-      <Route exact path="/">
-        <p>test</p>
-        <p>hello</p>
-      </Route>
-      <Route exact path="/lol">
-        <p>lol</p>
-      </Route>
-      <Route exact path="/test-component">
-        <TestComponent></TestComponent>
-      </Route>
-    </Router>
+    <Routes>
+      {/*  your routes here like:
+      <Route exact path="/meals" element={<TestComponent />} /> */}
+    </Routes>
   );
 }
-
+​
 export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import TestComponent from "./components/TestComponent/TestComponent";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Route exact path="/">
+//         <p>test</p>
+//         <p>hello</p>
+//       </Route>
+//       <Route exact path="/lol">
+//         <p>lol</p>
+//       </Route>
+//       <Route exact path="/test-component">
+//         <TestComponent></TestComponent>
+//       </Route>
+//     </Router>
+//   );
+// }
+
+// export default App;
