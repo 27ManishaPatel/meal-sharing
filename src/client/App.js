f0219af35a999bc5c4b7990be331d03df64f71b9
HackYourFuture DK <manisha270989@gmail.com>
2022-11-02 13:25:54 +0100

    updated package.json

--- src/client/App.js
+++ src/client/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
+import Home from "./Home";
+//import Navbar from "./Navbar";
+import Menu from './Menu';
+import Reservation from "./Reservation";
+import Review from "./Review";
+import Blog from "./Blog";
+
 function App() {
   return (
-    <Routes>
-  
-      <Route exact path="/meals" element={<TestComponent />} /> 
-      
-    </Routes>
+    <div>
+      <Routes>
+      <Route  exact path="/" element={<Home /> } />
+      <Route  exact path="Menu" element={<Menu /> } />
+      <Route  exact path="Reservation" element={<Reservation /> } />
+      <Route  exact path="Review" element={<Review /> } />
+      <Route  exact path="Blog" element={<Blog /> } />
+      </Routes>
+     
+    </div>
+
   );
 }
 export default App;
