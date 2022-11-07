6a5d99dccbce140eb654c58aede019aa95f3ced4
HackYourFuture DK <manisha270989@gmail.com>
2022-11-07 20:50:45 +0100

    testing

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
-import Home from "./Home";
-//import Navbar from "./Navbar";
-import Menu from './Menu';
-import Reservation from "./Reservation";
-import Review from "./Review";
-import Blog from "./Blog";
+
+
 
 function App() {
   return (
     <div>
       <Routes>
-      <Route  exact path="/" element={<Home /> } />
-      <Route  exact path="Menu" element={<Menu /> } />
-      <Route  exact path="Reservation" element={<Reservation /> } />
-      <Route  exact path="Review" element={<Review /> } />
-      <Route  exact path="Blog" element={<Blog /> } />
+      <Route  exact path="/" element={<TestComponent /> } />
       </Routes>
      
     </div>
