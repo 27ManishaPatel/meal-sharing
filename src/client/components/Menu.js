117d7cad201a291b34f7eeac2009215aad61149e
HackYourFuture DK <manisha270989@gmail.com>
2022-11-07 12:12:31 +0100

    rendered all meals

--- src/client/components/Menu.js
+++ src/client/components/Menu.js
@@ -4,40 +4,45 @@ import Header from "./Header";
 import Footer from "./Footer";
 import logo from './Images/images-3.png'
 
-
-
 export default function Menu() {
-    const [meal, setMeal] = useState([])
+    const [meal, setMeal] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(()=>{
-        (async()=>{
-            const result = await fetch('/api/meals').then(res => res.json())
-            console.log(result)
-            setMeal(result);
-
-        })()
+    const fetchMeals = () => {
+        setIsLoading(true);
+        fetch('/api/meals')
+            .then(res => res.json())
+            .then(meals => setMeal(meals))
+            .then(() => {
+                setIsLoading(false)
+            })
+    }
+    useEffect(() => {
+        fetchMeals();
     }, [])
 
     const ListOfMeals = meal.map(item => {
-            return (
-                <div className='meals'>
-                 <p key={item.id}>{item.title}</p>
-                 <p>Id: {item.id}</p>
-                 <img src={logo} width='200px'/>
-                 <p>{item.description}</p>
-                 <p>Price: {item.price}</p>
-                </div>
-           
-            )
-        })
-    
+        return (
+            <div className='meals'>
+                <p key={item.id} className='title'>{item.title}</p>
+                <p className='id'>Id: {item.id}</p>
+                <img className='meal-image' src={logo} />
+                <p className='description'>{item.description}</p>
+                <p className='price'>Price: {item.price}</p>
+            </div>
+        )
+    })
+
     return (
         <>
-             <Header/>
-             <div className='meal_container'>
-                {ListOfMeals}
-             </div>
-             <Footer />
+            <Header />
+            <div >
+                {isLoading && <p>Loading...</p>}
+                {meal.length === 0 ? <p>There is no meal available</p> :
+                <div className='meal_container'>{ ListOfMeals }</div> 
+                }
+            </div>
+            <Footer />
         </>
     )
 }
